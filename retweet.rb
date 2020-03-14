73b00d8ce81a0dacdac308be4b51a280cbb12e3c
kuma <kuma.atoz1009@gmail.com>
2020-03-14 19:00:55 +0900

    first commit

---
+++ retweet.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+
+# Config
+@client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["CONSUMER_TOKEN"]
+  config.access_token_secret = ENV["CONSUMER_TOKEN_SECRET"]
+end
+
+# PRODUCTION ENVIRONMENT -----------------------------------
+@client.list_timeline("nananekirinuki", "nanane").each do |tweet|
+    if tweet.user.screen_name != "nananekirinuki" && !tweet.retweeted? && !tweet.full_text.include?("specified word")
+        @client.retweet(tweet.id)
+    end
+end
